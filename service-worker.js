353aaf4d1d6b81dbddfe7377ca3ae5000c65df2d
Otakar81 <34288632+Otakar81@users.noreply.github.com>
2025-10-24 12:50:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Aggiornato per refresh immediato e cache controllata
 // ==========================
 
-const CACHE_NAME = "benessere-personale-cache-v10"; // cambiare questo valore a ogni rilascio
+const CACHE_NAME = "benessere-personale-cache-v11"; // cambiare questo valore a ogni rilascio
 const URLS_TO_CACHE = [
   "./",
   "index.html",
